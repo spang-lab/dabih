0cbd592202a2488cda09ebf3b4e8812a5da03207
Michael Huttner <michael@mhuttner.com>
2023-02-09 15:28:45 +0100

    change logging, combined dev command

--- src/middleware/logRequests.js
+++ src/middleware/logRequests.js
@@ -9,21 +9,13 @@ const timeToString = (timeMs) => {
 
 export default async (ctx, next) => {
   const start = Date.now();
-  let shortLog = true;
   const { url, method } = ctx.request;
   log(`-> Req ${url} ${method}`);
-  process.stdout.once('new_log', () => {
-    shortLog = false;
-  });
   await next();
   const timeMs = Date.now() - start;
 
   const time = timeToString(timeMs);
   const { status } = ctx.response;
-  if (shortLog) {
-    log.logInline(`${status} ${time}`);
-    return;
-  }
 
   log(`<- Res ${url} ${status} ${time}`);
 };
--- src/util/logger.js
+++ src/util/logger.js
@@ -14,21 +14,13 @@ const COLOR = {
 const logColor = (text, color) => {
   const time = new Date().toLocaleTimeString('en-US', { hour12: false });
   const rText = text.replace(/\n/g, '').replace(/\s+/g, ' ');
-  const arr = ['\n', COLOR.CYAN, time, color, ' > ', rText, COLOR.RESET];
-  process.stdout.emit('new_log');
+  const arr = [COLOR.CYAN, time, color, ' > ', rText, COLOR.RESET, '\n'];
   process.stdout.write(arr.join(''));
-};
-
-const logInline = (text) => {
-  const rText = text.replace(/\n/g, '').replace(/\s+/g, ' ');
-  process.stdout.emit('new_log');
-  process.stdout.write(` ${rText}`);
 };
 
 const log = (...arr) => logColor(arr.join(' '), COLOR.RESET);
 log.raw = (v) => process.stdout.write(`\n${v}`);
 log.log = log;
-log.logInline = (...arr) => logInline(arr.join(' '));
 log.warn = (...arr) => logColor(`Warning: ${arr.join(' ')}`, COLOR.YELLOW);
 log.error = (...arr) => logColor(`Error: ${arr.join(' ')}`, COLOR.RED);
 log.verbose = (...arr) => logColor(arr.join(' '), COLOR.MAGENTA);
