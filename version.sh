d8e95877a6b7d1564befcdcb806125fcb516e015
Michael Huttner <michael@mhuttner.com>
2023-08-21 11:49:31 +0200

    fix version script

--- version.sh
+++ version.sh
@@ -21,7 +21,7 @@ pushd client
   npm version --no-commit-hooks "$VERSION"
 popd
 pushd cli
-  $SED_COMMAND -i "s/version = .*/version = \"$VERSION\"/" Cargo.toml
+  $SED_COMMAND -i "s/^version = .*/version = \"$VERSION#v\"/" Cargo.toml
 popd
 
 
