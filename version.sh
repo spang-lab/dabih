37f9f272531c7d820a614ad6ebd1d965716b51be
Michael Huttner <michael@mhuttner.com>
2023-08-21 11:42:26 +0200

    update cli version aswell

--- version.sh
+++ version.sh
@@ -8,10 +8,23 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+if command -v gsed >/dev/null 2>&1; then
+    SED_COMMAND="gsed"
+else
+    SED_COMMAND="sed"
+fi
+echo $SED_COMMAND
+
+
 VERSION="$(npm version --no-git-tag-version "$1")"
 pushd client
   npm version --no-commit-hooks "$VERSION"
 popd
+pushd cli
+  $SED_COMMAND -i "s/version = .*/version = \"$VERSION\"/" Cargo.toml
+popd
+
+
 npm install  # update lockfile
 git add package{,-lock}.json ./**/*.json
 git commit --message "$VERSION"
