dd7aa37706ebf8501114450230b741c381eea232
Michael Huttner <michael@mhuttner.com>
2023-09-05 15:54:10 +0200

    fix eslint

--- client/components/api/Context.js
+++ client/components/api/Context.js
@@ -1,31 +1,31 @@
 'use client';
 
 /* eslint-disable no-console */
-import React, {createContext, useContext, useMemo} from 'react';
-import {useRouter, useParams} from 'next/navigation';
+import React, { createContext, useContext, useMemo } from 'react';
+import { useRouter, useParams } from 'next/navigation';
 
-import {useSession} from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 import axios from 'axios';
 
 const ApiContext = createContext();
 
-export function ApiWrapper({children}) {
+export function ApiWrapper({ children }) {
   const router = useRouter();
   const params = useParams();
-  const {data: session} = useSession();
+  const { data: session } = useSession();
 
   const api = axios.create();
   const getHeaders = (headers) => {
     if (params && params.token) {
-      const {token} = params;
+      const { token } = params;
       return {
         ...headers,
         Authorization: `Bearer dabih_${token}`,
       };
     }
     if (session && session.accessToken) {
-      const {provider, accessToken} = session;
+      const { provider, accessToken } = session;
       return {
         ...headers,
         Authorization: `Bearer ${provider}_${accessToken}`,
@@ -36,7 +36,7 @@ export function ApiWrapper({children}) {
 
   const onRequest = (config) => {
     const baseUrl = config.baseUrl || '/api/v1';
-    const {url, headers} = config;
+    const { url, headers } = config;
     const newUrl = `${baseUrl}${url}`;
 
     return {
@@ -51,19 +51,19 @@ export function ApiWrapper({children}) {
     (error) => {
       if (error.response.status === 401) {
         router.push('/logout');
-        return {error: 'Unauthorized'};
+        return { error: 'Unauthorized' };
       }
       const message = error.response.data || error.message;
       console.error(message);
-      return {error: message};
+      return { error: message };
     },
   );
 
   const admin = useMemo(
     () => ({
       listKeys: () => api.get('/admin/key/list'),
-      confirmKey: (keyId, confirmed) => api.post('/admin/key/confirm', {keyId, confirmed}),
-      deleteKey: (keyId) => api.post('/admin/key/remove', {keyId}),
+      confirmKey: (keyId, confirmed) => api.post('/admin/key/confirm', { keyId, confirmed }),
+      deleteKey: (keyId) => api.post('/admin/key/remove', { keyId }),
       listDatasets: () => api.get('/admin/dataset/list'),
       removeDataset: (mnemonic) => api.post(`/admin/dataset/${mnemonic}/remove`),
       destroyDataset: (mnemonic) => api.post(`/admin/dataset/${mnemonic}/destroy`),
@@ -87,10 +87,10 @@ export function ApiWrapper({children}) {
       listKeyUsers: () => api.get('/key/list/user'),
       getUser: async () => api.post('/token'),
       generateToken: async (type) => api.post(`/token/generate/${type}`),
-      removeToken: async (tokenId) => api.post('/token/remove', {tokenId}),
+      removeToken: async (tokenId) => api.post('/token/remove', { tokenId }),
       listTokens: async () => api.get('/token/list'),
       uploadCheck: async () => api.get('/upload/check'),
-      uploadStart: (fileName) => api.post('/upload/start', {fileName}),
+      uploadStart: (fileName) => api.post('/upload/start', { fileName }),
       uploadChunk: async (chunk, mnemonic) => {
         const {
           start, end, hash, data, totalSize,
@@ -105,20 +105,20 @@ export function ApiWrapper({children}) {
           'Content-Range': contentRange,
           'Content-Type': 'multipart/form-data',
         };
-        return api.put(url, formData, {headers});
+        return api.put(url, formData, { headers });
       },
       uploadFinish: async (mnemonic) => api.post(`/upload/finish/${mnemonic}`),
-      addPublicKey: async (publicKey) => api.post('/key/add', {publicKey}),
+      addPublicKey: async (publicKey) => api.post('/key/add', { publicKey }),
 
-      checkPublicKey: async (keyHash) => api.post('/key/check', {keyHash}),
+      checkPublicKey: async (keyHash) => api.post('/key/check', { keyHash }),
       searchDatasets: async (data) => api.post('/dataset/search', data),
-      fetchKey: async (mnemonic, keyHash) => api.post(`/dataset/${mnemonic}/key`, {keyHash}),
+      fetchKey: async (mnemonic, keyHash) => api.post(`/dataset/${mnemonic}/key`, { keyHash }),
       removeDataset: async (mnemonic) => api.post(`/dataset/${mnemonic}/remove`),
-      renameDataset: async (mnemonic, name) => api.post(`/dataset/${mnemonic}/rename`, {name}),
-      storeKey: async (mnemonic, key) => api.post(`/dataset/${mnemonic}/download`, {key}),
-      addDatasetMembers: async (mnemonic, members, key) => api.post(`/dataset/${mnemonic}/member/add`, {members, key}),
-      setMemberAccess: async (mnemonic, user, permission) => api.post(`/dataset/${mnemonic}/member/set`, {user, permission}),
-      reencryptDataset: async (mnemonic, key) => api.post(`/dataset/${mnemonic}/reencrypt`, {key}),
+      renameDataset: async (mnemonic, name) => api.post(`/dataset/${mnemonic}/rename`, { name }),
+      storeKey: async (mnemonic, key) => api.post(`/dataset/${mnemonic}/download`, { key }),
+      addDatasetMembers: async (mnemonic, members, key) => api.post(`/dataset/${mnemonic}/member/add`, { members, key }),
+      setMemberAccess: async (mnemonic, user, permission) => api.post(`/dataset/${mnemonic}/member/set`, { user, permission }),
+      reencryptDataset: async (mnemonic, key) => api.post(`/dataset/${mnemonic}/reencrypt`, { key }),
       fetchDataset: async (mnemonic) => api.get(`/dataset/${mnemonic}`),
       fetchChunk: async (mnemonic, chunkId) => api.get(`/dataset/${mnemonic}/chunk/${chunkId}`, {
         responseType: 'blob',
