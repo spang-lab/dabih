165df4cc9d31e28a57c16042857f6bfa6a0a2aaf
Michael Huttner <michael@mhuttner.com>
2024-01-26 09:53:56 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = [
   {
     name: 'api',
+    cwd: '/app/api',
     script: '/app/api/app.js',
   },
   {
-    name: 'nextjs',
+    name: 'next',
+    cwd: '/app/next',
     script: '/app/next/server.js',
   }
 ]
