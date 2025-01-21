1ab85d0278dccc64c2c05bbb7648deb8443e187e
Michael Huttner <michael@mhuttner.com>
2025-01-21 15:47:52 +0100

    cleanup

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,24 +1,7 @@
-import createMDX from '@next/mdx';
-import rehypeHighlight from 'rehype-highlight';
-import remarkFrontmatter from 'remark-frontmatter';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'standalone',
-  // rewrites: () => [
-  //   {
-  //     source: '/api/v1/:path*',
-  //     destination: 'http://localhost:3001/api/v1/:path*',
-  //   },
-  // ],
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx', 'md'],
+  output: "standalone",
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
 };
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    rehypePlugins: [rehypeHighlight],
-    remarkPlugins: [remarkFrontmatter],
-  },
-});
-export default withMDX(nextConfig);
+export default nextConfig;
